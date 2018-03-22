bce89272446709a3ccbef2a0e066e0fd2f199581
zj <1217336430@qq.com>
2018-03-22 17:16:33 +0800

    del lock.json

--- src/react.js
+++ src/react.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Component extends React.Component{ // eslint-disable-line
 	render() {
-		return <div>Helllo World</div> // eslint-disable-line
+		return <div>Helllo React</div> // eslint-disable-line
 	}
 }
 export default Component;
